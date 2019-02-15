73256cc968555f433f6ce4229f5dd6ad170cccfa
Ryo Nakamine <rnakamine8080@gmail.com>
2019-02-15 20:07:55 +0900

    initial commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+(function () {
+  new Vue({
+    el: '#app',
+    data() {
+      return {
+        info: null,
+        loading: true,
+        errored: false
+      }
+    },
+    mounted() {
+      axios
+        .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+        .then(response => (this.info = response.data.bpi))
+        .catch(error => {
+          console.log(error)
+          this.errored = true
+        })
+        .finally(() => this.loading = false)
+    },
+    filters: {
+      currencydecimal(value) {
+        return value.toFixed(2)
+      }
+    }
+  })
+})();
